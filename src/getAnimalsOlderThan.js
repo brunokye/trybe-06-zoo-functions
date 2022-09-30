3fad5231522ae5514e7e7a52dfeb98b08e5a0288
Bruno Ferreira <contatobrunokye@gmail.com>
2022-09-30 13:46:19 -0300

    Adiciona requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  let verifier = '';
+
+  species.filter((element) => {
+    if (element.name === animal) {
+      verifier = element.residents;
+    }
+
+    return verifier;
+  });
+
+  return verifier.every((number) => number.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;

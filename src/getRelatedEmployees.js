c9ce3186667d5c8cf1fe95c9b0dd91979ccab4db
Bruno Ferreira <contatobrunokye@gmail.com>
2022-09-30 19:47:28 -0300

    Adiciona requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((obj) => obj.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const filteredArray = employees.filter((obj) => obj.managers.includes(managerId));
+
+    return filteredArray.map((worker) => `${worker.firstName} ${worker.lastName}`);
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };

c4058551fa795984f4c23b13d0f54d39414f4c3e
Bruno Ferreira <contatobrunokye@gmail.com>
2022-10-02 18:42:28 -0300

    Adiciona requisito 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,6 +1,18 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
+  it('retorna undefined caso nenhum parâmetro seja passado', () => {
+    const actual = handlerElephants();
+    const expected = undefined;
+    expect(actual).toBe(expected);
+  });
+
+  it('retorna mensagem de erro caso o parâmetro não seja uma string', () => {
+    const actual = handlerElephants(1);
+    const expected = 'Parâmetro inválido, é necessário uma string';
+    expect(actual).toBe(expected);
+  });
+
   it('argumento count retorna 4', () => {
     const actual = handlerElephants('count');
     const expected = 4;

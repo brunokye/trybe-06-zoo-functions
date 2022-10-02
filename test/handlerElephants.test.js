cca2c02a2922d7c5ef5ad1fcc5f185540d6e02ec
Bruno Ferreira <contatobrunokye@gmail.com>
2022-10-02 18:34:22 -0300

    Adiciona requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,21 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('argumento count retorna 4', () => {
+    const actual = handlerElephants('count');
+    const expected = 4;
+    expect(actual).toBe(expected);
+  });
+
+  it('argumento names deve retornar um array de nomes que possui o nome Jefferson', () => {
+    const actual = handlerElephants('names');
+    const expected = 'Jefferson';
+    expect(actual).toContain(expected);
+  });
+
+  it('argumento averageAge deve retornar um número próximo a 10.5', () => {
+    const actual = handlerElephants('averageAge');
+    const expected = 10.5;
+    expect(actual).toBeCloseTo(expected);
+  });
+});

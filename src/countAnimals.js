443e45f34da58c501f2ad85c481e070bcf03587d
Bruno Ferreira <contatobrunokye@gmail.com>
2022-10-02 17:50:46 -0300

    Adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+
+function countAnimals(param) {
+  if (param === undefined) {
+    const newObj = {};
+
+    species.forEach(({ name, residents }) => { newObj[name] = residents.length; });
+    // species.forEach(({ name, residents }) => { Object.assign(newObj, { [name]: residents.length }); });
+
+    return newObj;
+  }
+
+  const { specie: animal, sex: gender = undefined } = param;
+  const filteredAnimal = species.find((obj) => obj.name === animal);
+
+  if (gender === undefined) return filteredAnimal.residents.length;
+
+  const filteredSex = filteredAnimal.residents.filter((arr) => arr.sex === gender);
+  return filteredSex.length;
 }
 
 module.exports = countAnimals;

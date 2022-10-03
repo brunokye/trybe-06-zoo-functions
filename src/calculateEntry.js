816f3d1cf9b5ec3773cab48865d5626f7231463f
Bruno Ferreira <contatobrunokye@gmail.com>
2022-10-03 17:43:55 -0300

    Adiciona requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const filteredChild = entrants.filter(({ age }) => age < 18).length;
+  const filteredAdult = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+  const filteredSenior = entrants.filter(({ age }) => age >= 50).length;
+  const stagesOfLife = {
+    child: filteredChild,
+    adult: filteredAdult,
+    senior: filteredSenior,
+  };
+
+  return stagesOfLife;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+
+  const priceChild = countEntrants(entrants).child * prices.child;
+  const priceAdult = countEntrants(entrants).adult * prices.adult;
+  const priceSenior = countEntrants(entrants).senior * prices.senior;
+  const sum = priceChild + priceAdult + priceSenior;
+
+  return sum;
 }
 
 module.exports = { calculateEntry, countEntrants };

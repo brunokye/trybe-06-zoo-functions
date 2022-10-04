dee7921038d05946624010df7100a4ce3fb40572
Bruno Ferreira <contatobrunokye@gmail.com>
2022-10-04 18:01:44 -0300

    Adiciona requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const filteredId = employees.find((obj) => obj.id === id);
+  const findAnimal = species.find((animal) => animal.id === filteredId.responsibleFor[0]);
+  const oldestAnimal = findAnimal.residents.reduce((a, b) => {
+    if (a.age > b.age) return a;
+    return b;
+  });
+
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;

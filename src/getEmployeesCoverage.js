5fa367b0485c382193ff5dcc5a6a4c1ffb9dffaf
Bruno Ferreira <contatobrunokye@gmail.com>
2022-10-30 17:15:09 -0300

    Atualiza o arquivo

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees } = data;
+
+const getAllInfo = (info) => {
+  let newObj = {};
+  const { id } = info;
+  const fullName = `${info.firstName} ${info.lastName}`;
+
+  newObj = { id, fullName };
+
+  return newObj;
+};
+
+function getEmployeesCoverage(employeeInfo) {
+  const { id: code, name: worker } = employeeInfo;
+
+  if (code === undefined && worker === undefined) return getAllInfo(employees);
+  // if (code === undefined);
+  // if (worker === undefined);
 }
+
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
